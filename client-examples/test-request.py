d6efe02a0503f095ae9f4d51c68365335a4c12ce
Fridolin-430 <fridolinfredy@gmail.com>
2025-06-23 14:34:28 +0400

    Delete client-examples/test-request.py

--- client-examples/test-request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BILLING_ID = "sample-record-id"
-API_URL = "https://<your-function-app-name>.azurewebsites.net/api/billing/{BILLING_ID}"
-
-response = requests.get(API_URL)
-print("Status Code:", response.status_code)
-print("Response:", response.json())
